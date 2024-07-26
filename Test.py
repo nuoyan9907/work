52a567c47d5da6c3ab1ef10f7d5013c9c03f251c
nuoyan9907 <nuoyan9907@gmail.com>
2024-07-26 11:37:49 +0800

    Hello

---
+++ Test.py
@@ -0,0 +1,10 @@
+# Simple Python script
+
+# Print a greeting message
+print("Hello, World!")
+
+# Ask the user for their name
+name = input("What's your name? ")
+
+# Print a personalized greeting
+print(f"Nice to meet you, {name}!")
